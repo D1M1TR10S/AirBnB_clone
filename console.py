6649f560a59d09a0ac4a3c253cfedccba6ff6e0d
Lance Wong <lwong180@gmail.com>
2018-02-12 19:18:48 +0000

    typecast update val, use shlex.split instead of split() method

--- console.py
+++ console.py
@@ -2,10 +2,19 @@
 """Command interpreter module."""
 
 import cmd
+from shlex import shlex
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models import storage
 
-classes = {'BaseModel': BaseModel}
+classes = {'BaseModel': BaseModel, 'User': User, 'State': State,
+           'City': City, 'Amenity': Amenity, 'Place': Place,
+           'Review': Review}
 
 
 class HBNBCommand(cmd.Cmd):
@@ -18,7 +27,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, arg):
         """Creates new instance of BaseModel"""
-        args = arg.split()
+        args = shlex.split(arg)
         if self.check_args(args, 'create') == 0:
             new = classes[args[0]]()
             new.save()
@@ -34,30 +43,38 @@ class HBNBCommand(cmd.Cmd):
 
     def do_show(self, arg):
         """Prints string representation"""
-        args = arg.split()
+        args = shlex.split(arg)
         if self.check_args(args, 'show') == 0:
             print(storage.all()[args[0] + '.' + args[1]])
 
     def do_destroy(self, arg):
         """Deletes an instance based on the class name and id"""
-        args = arg.split()
+        args = shlex.split(arg)
         if self.check_args(args, 'destroy') == 0:
             del storage.all()[args[0] + '.' + args[1]]
             storage.save()
 
     def do_all(self, arg):
         """Prints string representations of all instances of a class"""
-        args = arg.split()
+        args = shlex.split(arg)
         if self.check_args(args, 'all') == 0:
             print([str(value) for key, value in storage.all().items()
                   if arg in key])
 
     def do_update(self, arg):
         """Updates an instance."""
-        args = arg.split()
+        args = shlex.split(arg)
         if self.check_args(args, 'update') == 0:
+            v = args[3]
+            type_d = {'number_rooms': int(v), 'number_bathrooms': int(v),
+                      'max_guest': int(v), 'price_by_night': int(v),
+                      'latitude': float(v), 'longitude': float(v),
+                      'email': v, 'password': v, 'first_name': v,
+                      'last_name': v, 'name': v, 'state_id': v, 'city_id':
+                      v, 'user_id': v, 'description': v, 'place_id': v,
+                      'text': v}
             key = '{}.{}'.format(args[0], args[1])
-            setattr(storage.all()[key], args[2], args[3])
+            setattr(storage.all()[key], args[2], type_d[args[2]])
 
     @staticmethod
     def check_args(args, cmd):
