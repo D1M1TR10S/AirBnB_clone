8c817744db71424c22a18a00335978422ea85cde
Lance Wong <lwong180@gmail.com>
2018-02-15 02:07:06 +0000

    file_storage more tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Unittest for file storage."""
 import unittest
+import os.path
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
 
 class TestFileStorage(unittest.TestCase):
@@ -14,3 +16,29 @@ class TestFileStorage(unittest.TestCase):
             print(a.file_path)
         with self.assertRaises(AttributeError):
             print(a.objects)
+
+    def test_type(self):
+        """Check if methods return expected type."""
+        a = FileStorage()
+        self.assertTrue(type(a.all()) is dict)
+
+    def test_new(self):
+        """Tests if new method adds obj to __objects."""
+        a = FileStorage()
+        b = BaseModel()
+        a.new(b)
+        self.assertTrue('BaseModel.' + b.id in a.all())
+
+    def test_save(self):
+        """Tests that save method does what I told it to do."""
+        a = FileStorage()
+        a.save()
+        self.assertTrue(os.path.exists('file.json'))
+
+    def test_reload(self):
+        """Make sure __objects is reloaded properly."""
+        a = FileStorage()
+        a.save()
+        a.reload()
+        for values in a.all().values():
+            self.assertTrue(values is not dict)
